94d3f85b9980aa5caf730e38af7727d76bb2eff9
Pepe <josegarriaga7@gmail.com>
2021-08-24 18:02:39 +0200

    photo project

--- src/components/DataProjects.js
+++ src/components/DataProjects.js
@@ -1,14 +1,14 @@
 let projectsInfo = [
   {
     id: 1,
-    img: "images/occycling.png",
+    img: "images/occycling.jpg",
     title: "OCCYCLING",
     deployed: "Have not yet deployed",
     projectName: "Occycling",
     tools:
       "React | Javascript | ReactBoostrap | Express | Node.Js | Postman | PostgreSql | HTML5 | CSS3 | JWT",
     description:
-      "A platform to manage the free bicycle provision service given by OCC in a refugee camp in Greece to allow them to go to the city.",
+      "A platform to manage the free bicycle provision service given by OCC in a refugee camp in Greece to allow them to go to the city. The purpose of estimating benefits for this project is to record, measure and consult  information for decision-making wich leads to give a better service for users.",
     readMore: "https://openculturalcenter.org/es/occycling-2/",
   },
   {
